486830f1be38e709937d24c2412dd9099699bef5
AndreiYakerson <tubikwheels@gmail.com>
2025-09-19 23:17:36 +0300

    Add mongo DB for prod

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 export default {
-  dbURL: process.env.MONGODB_URL,
+  //TODO Mongo DB connection
+  dbURL: process.env.MONGODB_URL || 'mongodb+srv://tubikwheels_db_user:ZUUHCBQDIxJpo7UM@toy.atxg3w7.mongodb.net/',
   dbName: 'toy_db',
 }
